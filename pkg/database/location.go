82029de4089cfc121c38945e1b27931394dafb26
Ryan McCrystal <rrmccrystal@gmail.com>
2020-09-15 21:50:09 -1000

    Added GetLocationByID

--- pkg/database/location.go
+++ pkg/database/location.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 // A Location represents any location at the school that can be signed in or out
@@ -23,6 +24,27 @@ func (db *Database) CreateLocation(location *Location) error {
 	location.ID = result.InsertedID.(primitive.ObjectID)
 
 	return nil
+}
+
+// GetLocationByID gets a location by its ID. If not found, found will be false and
+// err will be nil. If there is an error retrieving the location, it will be returned
+func (db *Database) GetLocationByID(id string) (location Location, found bool, err error) {
+	result := db.Collections.Locations.FindOne(nil, bson.M{"_id": id})
+
+	err = result.Err()
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return Location{}, false, nil
+		}
+		return Location{}, false, err
+	}
+
+	if err := result.Decode(&location); err != nil {
+		return Location{}, true, err
+	}
+
+	found = true
+	return
 }
 
 // GetLocations returns a list of all locations stored in the database.
