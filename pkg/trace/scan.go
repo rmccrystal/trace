d89649165b4a021a1d331a8b6729bcce47cae6b9
Ryan McCrystal <rrmccrystal@gmail.com>
2020-09-16 09:15:54 -1000

    Update scan.go

--- pkg/trace/scan.go
+++ pkg/trace/scan.go
@@ -2,6 +2,7 @@ package trace
 
 import (
 	"fmt"
+	"github.com/sirupsen/logrus"
 	"time"
 	"trace/pkg/database"
 )
@@ -9,40 +10,37 @@ import (
 // HandleScan should be called whenever a student scans in or scans out.
 // It will return the Events that it creates or an error.
 // If the studentID cannot be found in the database, it will not be stored
-func HandleScan(locationID string, studentHandle string) (database.Event, error) {
+// If there is an error with the input (locationID or studentHandle is invalid), the
+// error will be returned as a userError. If there is an error accessing the database
+// or any other unexpected error, it will be returned in err
+func HandleScan(locationID string, studentHandle string) (ev database.Event, userError error, err error) {
 	location, found, err := database.DB.GetLocationByID(locationID)
 	if err != nil {
-		return database.Event{}, err
+		return database.Event{}, nil, err
 	}
 	if !found {
-		return database.Event{}, fmt.Errorf("location with ID %s was not found", locationID)
+		return database.Event{}, fmt.Errorf("location with ID %s was not found", locationID), nil
 	}
 
 	student, found, err := database.DB.GetStudentByHandle(studentHandle)
 	if err != nil {
-		return database.Event{}, err
+		return database.Event{}, nil, err
 	}
 	if !found {
-		return database.Event{}, fmt.Errorf("student with handle %s was not found", studentHandle)
+		return database.Event{}, fmt.Errorf("student with handle %s was not found", studentHandle), nil
 	}
 
-	// If the student is entering or leaving the location
+	studentAtLocation, err := IsStudentAtLocation(student.ID, location.ID)
+	if err != nil {
+		return database.Event{}, nil, fmt.Errorf("encountered error checking if student %s is at location %s", student.Name, location.Name)
+	}
+
 	var eventType database.EventType
-
-	lastEvent, found, _ := database.DB.GetMostRecentEvent(student.ID)
-
-	// If there are no past events, assume the student is entering the location
-	if !found {
-		eventType = database.EventEnter
+	// If the student is in the location, they are leaving, otherwise they are entering
+	if studentAtLocation {
+		eventType = database.EventLeave
 	} else {
-		// If the student last entered the library, they are leaving
-		// TODO: If the student entered the library like a day ago then they should be entering again
-		switch lastEvent.EventType {
-		case database.EventLeave:
-			eventType = database.EventEnter
-		case database.EventEnter:
-			eventType = database.EventLeave
-		}
+		eventType = database.EventEnter
 	}
 
 	event := database.Event{
@@ -54,8 +52,19 @@ func HandleScan(locationID string, studentHandle string) (database.Event, error)
 	}
 
 	if err := database.DB.CreateEvent(&event); err != nil {
-		return database.Event{}, fmt.Errorf("error creating event: %s", err)
+		return database.Event{}, nil, fmt.Errorf("error creating event: %s", err)
 	}
 
-	return event, nil
+	// Log the event
+	var evName string
+	if eventType == database.EventEnter {
+		evName = "entered"
+	} else if eventType == database.EventLeave {
+		evName = "left"
+	}
+	logrus.WithFields(logrus.Fields{
+		"studentName": student.Name, "locationName": location.Name,
+	}).Debugf("Student %s the library", evName)
+
+	return event, nil, nil
 }
