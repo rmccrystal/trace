9245a7f15bd19c82f438506d11de35692fcf0e58
Ryan McCrystal <rrmccrystal@gmail.com>
2020-09-16 21:23:25 -1000

    Added tests for API (TRACE-5)

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.14
 
 require (
 	github.com/gin-gonic/gin v1.6.3
+	github.com/go-playground/assert/v2 v2.0.1
 	github.com/sirupsen/logrus v1.4.2
+	github.com/stretchr/testify v1.4.0
 	go.mongodb.org/mongo-driver v1.4.1
 )
--- pkg/controllers/controllers_test.go
+++ pkg/controllers/controllers_test.go
@@ -1,18 +1,97 @@
 package controllers
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
+	"trace/pkg/database"
 )
 
-func TestOnScan(t *testing.T) {
+var TestDatabase *database.Database
+
+var TestStudent *database.Student
+var TestLocation *database.Location
+
+func init() {
+	logrus.SetLevel(logrus.TraceLevel)
+
+	var err error
+
+	TestDatabase, err = database.Connect(database.Config{
+		MongoURI:     "mongodb://localhost",
+		DatabaseName: "tests",
+	})
+
+	if err != nil {
+		logrus.Fatalf("Could not connect to database: %s", err)
+	}
+
+	// Purge the test database
+	_ = TestDatabase.Database.Drop(nil)
+	logrus.Infof("Purged the tests database")
+
+	// Add an example student for the test
+	TestStudent = &database.Student{
+		Name:           "Ben Aaron",
+		Email:          "baaron@gmail.com",
+		StudentHandles: []string{"testhandle"},
+	}
+	if err := TestDatabase.CreateStudent(TestStudent); err != nil {
+		logrus.Fatalf("Error adding student to the database: %s", err)
+		TestDatabase = nil
+	}
+
+	TestLocation = &database.Location{
+		Name:    "Library",
+		Timeout: 1 * time.Hour,
+	}
+	if err := TestDatabase.CreateLocation(TestLocation); err != nil {
+		logrus.Fatalf("Error creating location: %s", err)
+		TestDatabase = nil
+	}
+}
+
+func sendTestRequest(handler func(ctx *gin.Context), json []byte) (code int, body []byte) {
 	w := httptest.NewRecorder()
 	c, _ := gin.CreateTestContext(w)
 
-	m := json.
-	httptest.NewRequest("POST", "/api/v1/scan", nil)
+	// The method and url don't matter because we're running the handler function directly
+	req, _ := http.NewRequest("GET", "/", bytes.NewBuffer(json))
+	req.Header.Set("Content-Type", "application/json")
 
-	OnScan(c)
+	c.Request = req
+
+	handler(c)
+
+	body, _ = ioutil.ReadAll(w.Body)
+
+	return w.Code, body
+}
+
+func TestOnScan(t *testing.T) {
+	// I haven't ran this test yet because my docker isn't working on my PC for some reason
+	// TODO: Run this test
+	code, resp := sendTestRequest(OnScan, []byte(fmt.Sprintf(`
+{
+	"student_handle": "testhandle",
+	"location_id": "%s"
+}`, TestLocation.ID.Hex())))
+
+	assert.Equal(t, code, 200)
+
+	var createdEvent database.Event
+	assert.NoError(t, json.Unmarshal(resp, &createdEvent), "failed to unmarshal response")
+
+	mostRecentEvent, found, _ := TestDatabase.GetMostRecentEvent(TestStudent.ID)
+	assert.Truef(t, found, "no event was created")
+	assert.Equalf(t, mostRecentEvent.ID, createdEvent.ID, "the returned event id %s did not match the most recent event id %s", createdEvent.ID, mostRecentEvent.ID)
+	assert.Equal(t, createdEvent.EventType, database.EventEnter, "incorrect event type %s was created", createdEvent.EventType)
 }
