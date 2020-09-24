7817964c2a868733e1359f2a934de90184bcbe3b
Ryan McCrystal <rrmccrystal@gmail.com>
2020-09-24 13:00:41 -1000

    Update dashboard.tsx

--- frontend/src/components/dashboard.tsx
+++ frontend/src/components/dashboard.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import {Card, Spinner} from "@blueprintjs/core";
+import {Button, Card, HTMLTable, Spinner} from "@blueprintjs/core";
 import {getStudentsAtLocation, Student} from "../api";
 import {useGlobalState} from "../app";
 import {onCatch} from "./util";
@@ -28,8 +28,21 @@ export default function Dashboard() {
         return <Spinner className="mt-10"/>
     }
 
-    return <Card className="max-w-xl m-auto mt-8">
-        <h1 className="bp3-heading">Students in {location?.name}</h1>
-        {students.map(student => <div className="bp3-text-large">{student.name}</div>)}
+    return <Card className="max-w-xl w-full m-8">
+        <h1 className="bp3-heading text-center">Currently in {location?.name} ({students.length})</h1>
+        <HTMLTable condensed striped className="w-full">
+            <thead>
+                <th>Name</th>
+                <th>Time Elapsed</th>
+                {/*<th>Log Out</th>*/}
+            </thead>
+            <tbody>
+            {students.map(student => <tr>
+                <td style={{verticalAlign: "middle"}}>{student.name}</td>
+                <td style={{verticalAlign: "middle"}}>1 hour</td>
+                <td><Button icon="delete" className="float-right" minimal text={`Log out`}/></td>
+            </tr>)}
+            </tbody>
+        </HTMLTable>
     </Card>
 }
