91520699ef668bdc1c7437863c881d51457d29f2
Ryan McCrystal <rrmccrystal@gmail.com>
2020-09-29 17:03:01 -1000

    Get config from env variables

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/sirupsen/logrus"
+	"os"
 	"time"
 	"trace/pkg/api"
 	"trace/pkg/database"
@@ -10,11 +11,14 @@ import (
 func main() {
 	logrus.SetLevel(logrus.DebugLevel)
 
-	addr := "0.0.0.0:8080"
+	addr := envOr("LISTEN_ADDRESS", "0.0.0.0:8080")
+	mongoUri := envOr("MONGO_URI", "mongodb://localhost")
+	databaseName := envOr("DATABASE_NAME", "dev")
+
 	config := api.Config{
 		DatabaseConfig: database.Config{
-			MongoURI:     "mongodb://localhost",
-			DatabaseName: "dev",
+			MongoURI:     mongoUri,
+			DatabaseName: databaseName,
 		},
 		Timeout:        3 * time.Hour,
 	}
@@ -28,4 +32,13 @@ func main() {
 	if err := api.Listen(addr, &config); err != nil {
 		logrus.Fatalf("Failed to listen on %s: %s", addr, err)
 	}
+}
+
+// envOr returns an env variable by its key or the defaultValue if it is not found
+func envOr(key string, defaultValue string) string {
+	value, found := os.LookupEnv(key)
+	if !found {
+		return defaultValue
+	}
+	return value
 }
